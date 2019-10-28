36eea82ca3e57315231154f0dcb46b8c27c807c6
Rusyanto Rusanto <rusyanto@gmail.com>
2019-10-28 22:55:59 +0800

    Completed registration form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from '../../assets/logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import domo from 'ryuu.js';
+import Registration from '../Registration';
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiButton: {
+      contained: {
+        backgroundColor: '#fc8f13',
+        color: 'white',
+        '&:hover': {
+          backgroundColor: '#ed8003',
+        }
+      },
+    }
+  },
+  palette: {
+    text: {
+      primary: 'rgba(0, 0, 0, 0.8)',
+      secondary: 'rgba(0, 0, 0, 0.4)'
+    }
+  }
+});
 
 function App() {
+  useEffect(() => {
+    domo.get(`/domo/datastores/v1/collections/`)
+      .then(data => {})
+      .catch(e => {
+        domo.post(`/domo/datastores/v1/collections`, { "name": "ClinicRegistration" })
+          .then(data => console.log(data));
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Registration />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import domo from 'ryuu.js';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing(1)
+  }
+}));
+
+function Registration() {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    name: '',
+    email: '',
+    userType: '',
+    interest: '',
+    question: ''
+  });
+  const [status, setStatus] = React.useState('notSubmitted');
+
+  const inputLabel = React.useRef(null);
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    setStatus('submitting');
+    domo.post(`/domo/datastores/v1/collections/ClinicRegistration/documents/`, {
+      "content": values
+    }).then(data => setStatus('submitted'));
+  }
+
+  if (status !== 'submitted') {
+    return (
+      <form className={classes.container} autoComplete="off" onSubmit={handleSubmit}>
+        <Typography variant="h6" gutterBottom className={classes.formControl}>
+          Domo Clinic Registration
+        </Typography>
+        <TextField
+          required
+          id="input-name"
+          label="Name"
+          value={values.name}
+          onChange={handleChange('name')}
+          className={classes.formControl}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          type="text"
+        />
+        <TextField
+          required
+          id="input-email"
+          label="Email"
+          value={values.email}
+          onChange={handleChange('email')}
+          className={classes.formControl}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+          type="email"
+        />
+        <FormControl variant="outlined" className={classes.formControl} fullWidth>
+          <InputLabel ref={inputLabel} htmlFor="input-user-type">
+            What type of user are you?
+          </InputLabel>
+          <Select
+            value={values.userType}
+            onChange={handleChange('userType')}
+            labelWidth={200}
+            inputProps={{
+              name: 'userType',
+              id: 'input-user-type'
+            }}
+          >
+            <MenuItem value={'Technical User'}>Technical User</MenuItem>
+            <MenuItem value={'Business User'}>Business User</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl variant="outlined" className={classes.formControl} fullWidth>
+          <InputLabel ref={inputLabel} htmlFor="input-interest">
+            What are you interested in learning about?
+          </InputLabel>
+          <Select
+            value={values.interest}
+            onChange={handleChange('interest')}
+            labelWidth={310}
+            inputProps={{
+              name: 'interest',
+              id: 'input-interest'
+            }}
+          >
+            <MenuItem value={'Collaboration'}>Collaboration</MenuItem>
+            <MenuItem value={'Buzz'}>Buzz</MenuItem>
+            <MenuItem value={'Chart Properties'}>Chart Properties</MenuItem>
+            <MenuItem value={'Building Charts'}>Building Charts</MenuItem>
+            <MenuItem value={'Domo Stories'}>Domo Stories</MenuItem>
+            <MenuItem value={'Data Connection'}>Data Connection</MenuItem>
+            <MenuItem value={'Magic ETL'}>Magic ETL</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          id="outlined-multiline-static"
+          label="Do you have any specific questions to be covered during the Q&A?"
+          value={values.question}
+          onChange={handleChange('question')}
+          className={classes.formControl}
+          multiline
+          rows="3"
+          margin="normal"
+          variant="outlined"
+          fullWidth
+        />
+        { status !== 'submitting'
+          ?
+          <Button variant="contained" type="submit" className={classes.formControl}>
+            Submit
+          </Button>
+          :
+          <CircularProgress className={classes.formControl} />
+        }
+      </form>
+    );
+  } else {
+    return (
+      <Typography variant="h5" gutterBottom align='center' style={{marginTop: 75}}>
+        Thank you for registering!
+      </Typography>
+    );
+  }
+}
+
+export default Registration;
